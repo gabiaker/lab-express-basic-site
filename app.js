55f1bf87c70265057be275ed4b7f5a9d6bbc02c0
John Doe <gabiak+stack@gmail.com>
2020-06-26 18:43:16 +0200

    routing works

--- app.js
+++ app.js
@@ -2,29 +2,33 @@ const express = require('express');
 
 const app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
 app.use(express.static('public'));
 
-app.get('/', (request, response, next) => {
+app.get('/', (request, response) => {
     console.log(request);
-    response.send('/index.html');
+    response.sendFile(__dirname + '/views/homepage.html');
+  });
+
+  app.get('/homepage', (request, response) => {
+    console.log(request);
+    response.sendFile(__dirname + '/views/homepage.html');
+  });
+
+  app.get('/photos', (request, response) => {
+    response.sendFile(__dirname + `/views/photos.html`);
+  });
+
+  app.get('/about', (request, response) => {
+    response.sendFile(__dirname + `/views/about.html`);
+  });
+
+  app.get('/works', (request, response) => {
+    response.sendFile(__dirname + `/views/works.html`);
   });
 
   app.listen(3000, () => {
     console.log('My first app listening on port 3000!')
-  });
-
-  app.get('/works', (request, response, next) => {
-    response.send(`/views/works.html`);
-  });
-
-  app.get('/views/photos.html', (request, response, next) => {
-    response.send(`/views/photos.html`);
-  });
-
-  app.get('/about', (request, response, next) => {
-    response.send(`/views/about.html`);
-  });
-
-  app.get('/works', (request, response, next) => {
-    response.send(`/views/works.html`);
   });
